0ec96ad217e1044c8cb3a114e40346c98890ed2f
Edvin Mocibob <edvin.mocibob@gmail.com>
2015-06-23 14:49:42 +0200

    Maknuta suvišna prazna linija.

--- src/napraviDir.m
+++ src/napraviDir.m
@@ -2,7 +2,6 @@ function [dirZaOkvire] = napraviDir( ime )
 % kreiraj novi direktorij (ako veæ ne postoji) i vrati njegovo
 
 dirZaOkvire = fullfile(ime);
-
 if ~exist(dirZaOkvire, 'dir')
    mkdir(dirZaOkvire) 
 end
