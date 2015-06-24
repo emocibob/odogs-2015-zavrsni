8b58393060782d2d666a5c113af2c8f7f9ee967f
Edvin Mocibob <edvin.mocibob@gmail.com>
2015-06-24 13:33:52 +0200

    Dodana opcija za spremanje slike i druge manje izmjene.

--- src/segmKMeans.m
+++ src/segmKMeans.m
@@ -1,13 +1,12 @@
-function segmKMeans( dirZaOkvire, ime, primjeniBlur )
+function segmKMeans( dirZaOkvire, imeOkvira, brBoja, primjeniBlur, pokaziRez, spremi )
 % automatska segmentacija boja na temelju L*a*b* modela boja i k-means grupiranja
 
 % proèitaj sliku i po potrebi je zamuti
-dat = fullfile(dirZaOkvire, ime);
+dat = fullfile(dirZaOkvire, imeOkvira);
 okvir = imread(dat);
 if primjeniBlur == true
-   okvir = blurFilter(okvir, 10, false);
+   okvir = blurFilter(okvir, 4, false);
 end
-imshow(okvir), title('Test okvir');
 
 % pretvaranje iz RGB u L*a*b* model boja
 cform = makecform('srgb2lab');
@@ -18,13 +17,14 @@ ab = double(labOkvir(:, :, 2:3));
 brRedaka = size(ab, 1);
 brStupaca = size(ab, 2);
 ab = reshape(ab, brRedaka*brStupaca, 2);
-brBoja = 4;
-[cluster_idx, cluster_center] = kmeans(ab,brBoja, 'distance','sqEuclidean', 'Replicates',3);
+[cluster_idx, cluster_center] = kmeans(ab, brBoja, 'distance','sqEuclidean', 'Replicates',3);
 
-% oznaèi svaki piksel na temelju rezultata grupiranja
+% oznaèi svaki piksel na temelju rezultata grupiranja i po potrebi prikaži nastalu mapu
 pixelOznake = reshape(cluster_idx, brRedaka, brStupaca);
-imshow(pixelOznake, []);
-title(['Okvir ', ime, ' oznaèen indeksima pripadnih klastera']);
+if pokaziRez == true
+    figure;
+    imshow(pixelOznake, []);
+end
 
 % napravi slike koje pokazuju segmentirana podruèja
 segmentiraneSlike = cell(1, brBoja);
@@ -34,10 +34,26 @@ for k = 1:brBoja
     boja(rgbOznaka ~= k) = 0;
     segmentiraneSlike{k} = boja;
 end
-for k = 1:brBoja
-    figure;
-    imshow(segmentiraneSlike{k});
-    title(['Objekti klastera br. ', num2str(k)]);
+
+% prikaži segmentirane slike (ako je tako odreðeno)
+if pokaziRez == true
+    for k = 1:brBoja
+        figure;
+        imshow(segmentiraneSlike{k});
+        title(['Objekti grupe br. ', num2str(k)]);
+    end
+end
+
+% spremi segmentirane slike
+if spremi == true
+    for k = 1:brBoja
+        if primjeniBlur == true
+            sufiks = ['blur_k_', num2str(brBoja), '_g_', num2str(k)];
+        else
+            sufiks = ['k_', num2str(brBoja), '_g_', num2str(k)];
+        end
+        spremiSliku(segmentiraneSlike{k}, 'okviri_segm_kmeans', imeOkvira, sufiks);
+    end
 end
 
 end
